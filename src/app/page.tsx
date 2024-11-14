6336d625933d1e2eec35f981d5e5688ae26aaf98
rohanmoura <rsmoura29@gmail.com>
2024-11-14 10:51:59 +0530

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button onClick={() => alert("Hello!")}>
-      Hello World
-    </Button>
+    <>
+      <Input />
+      <Button variant={"primary"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+      <Button variant={"destructive"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+      <Button variant={"secondary"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+      <Button variant={"ghost"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+      <Button variant={"muted"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+      <Button variant={"teritary"} onClick={() => alert("Hello!")}>
+        Hello World
+      </Button>
+    </>
   );
 }
